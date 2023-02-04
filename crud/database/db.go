ec658928ada83a24f89cd5dead9faa2952af633e
Rajat Kumar <rajat.kumar@ventura1.com>
2023-02-04 12:00:50 +0530

    [feat] readme updated and postman base URL changed

--- crud/database/db.go
+++ crud/database/db.go
@@ -4,14 +4,13 @@ import (
 	"context"
 	"github.com/jackc/pgx/v5"
 	"log"
-	"os"
 )
 
 var CRUD_DB *pgx.Conn
 
 func InitDB() {
 	var err error
-	if CRUD_DB, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL")); err != nil {
+	if CRUD_DB, err = pgx.Connect(context.Background(), "postgresql://postgres:U0JxW4N6PzPkeCaff0RW@containers-us-west-45.railway.app:7316/railway"); err != nil {
 		log.Fatalf("Error fetching DB, %s", err.Error())
 	}
 	log.Println("Connected to DB now")
