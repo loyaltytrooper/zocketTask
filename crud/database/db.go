6cc0c3c068fa3bf90aeaae31d73fa672a74ebdd1
Rajat Kumar <89660252+loyaltytrooper@users.noreply.github.com>
2023-02-04 13:37:48 +0530

    [feat] re-added environment variable DB URL

--- crud/database/db.go
+++ crud/database/db.go
@@ -10,7 +10,7 @@ var CRUD_DB *pgx.Conn
 
 func InitDB() {
 	var err error
-	if CRUD_DB, err = pgx.Connect(context.Background(), "postgresql://postgres:U0JxW4N6PzPkeCaff0RW@containers-us-west-45.railway.app:7316/railway"); err != nil {
+	if CRUD_DB, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"); err != nil {
 		log.Fatalf("Error fetching DB, %s", err.Error())
 	}
 	log.Println("Connected to DB now")
